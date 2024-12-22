163107e514525ef8bd5e423743b5f89d862dc560
Youssef Elharty <youssefashraf246@gmail.com>
2024-12-22 23:15:43 +0200

    Added: endpoints for testing auth

--- src/api/routes/routes-index.js
+++ src/api/routes/routes-index.js
@@ -4,6 +4,8 @@ import { serve, setup } from 'swagger-ui-express';
 import { specs, swaggerConfig } from '../../config/config.js';
 import patientRouter from './patient-route.js';
 import doctorRouter from './doctor-route.js';
+import { authAccessToken } from '../middleware/auth-middleware.js';
+import { authorizeUser } from '../middleware/access-middleware.js';
 const router = Router();
 
 const specDoc = swaggerJsdoc(swaggerConfig);
@@ -12,6 +14,12 @@ router.use(specs, serve);
 router.get(specs, setup(specDoc, { explorer: true }));
 
 // User routes
+router.get('/check-authentication', authAccessToken, (req, res, next) => {
+    res.send({authenticated: true});
+});
+router.get('/check-authorization', authAccessToken, authorizeUser, (req, res, next) => {
+    res.send({authorized: true, role: req.user.role});
+});
 router.use('/patient', patientRouter);
 router.use('/doctor', doctorRouter);
 
