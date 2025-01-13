c5151a001b0f8bcced2a18213360a2781f49ec51
Youssef Elharty <youssefashraf246@gmail.com>
2025-01-13 18:46:21 +0200

    Modified: auth middleware

--- src/api/middleware/auth-middleware.js
+++ src/api/middleware/auth-middleware.js
@@ -4,5 +4,5 @@ import { auth0_audience, auth0_domain } from '../../config/config.js';
 // Middleware to validate JWTs issued by Auth0
 export const authAccessToken = auth({
   audience: auth0_audience,
-  issuerBaseURL: auth0_domain,
+  issuerBaseURL: `https://${auth0_domain}`,
 });
