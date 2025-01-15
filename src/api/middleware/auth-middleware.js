9545a61c755795205be07a55c1f3280f8bdee256
Mohamed Elshaarawy <elsharawy234@gmail.com>
2025-01-15 21:43:30 +0200

    Added: return 401

--- src/api/middleware/auth-middleware.js
+++ src/api/middleware/auth-middleware.js
@@ -2,7 +2,16 @@ import { auth } from 'express-oauth2-jwt-bearer';
 import { auth0_audience, auth0_domain } from '../../config/config.js';
 
 // Middleware to validate JWTs issued by Auth0
-export const authAccessToken = auth({
+const authAccessToken = auth({
   audience: auth0_audience,
   issuerBaseURL: `https://${auth0_domain}`,
 });
+
+export const authenticate = (req, res, next) => {
+  authAccessToken(req, res, err => {
+    if (err) {
+      return res.status(401).send('Unauthorized');
+    }
+    return next();
+  });
+};
