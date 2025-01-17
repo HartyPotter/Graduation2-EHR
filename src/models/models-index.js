460b16574b87432799adcc4607619db1815fbb7d
Mohamed Elshaarawy <elsharawy234@gmail.com>
2025-01-17 02:28:31 +0200

    fix import

--- src/models/models-index.js
+++ src/models/models-index.js
@@ -6,7 +6,7 @@ import Contact from './emergency-contact-model.js';
 import PatientContact from './patient-contact-model.js';
 import Hospital from './hospital-model.js';
 import Admin from './admin-model.js';
-import Admission from './admission-model.js';
+import HospitalDoctor from './hospital-doctor-model.js';
 
 // Define many-to-many relationship
 Patient.belongsToMany(Contact, { through: PatientContact, foreignKey: 'patient_id' });
